5db80d0d2206d14f23fb88cc080cd24dbb076967
Vincent <vincentbolinget@gmail.com>
2024-09-24 16:30:07 +0800

    additional files

---
+++ fetch-provinces.php
@@ -0,0 +1,29 @@
+<?php 
+    require_once 'Database.php';
+
+    try{
+        $db = new Database('localhost', 'root', '', 'classhub_data');
+    } catch(Exception $e){
+        http_response_code(500);
+        echo json_encode(["error" => "Failed to connect to the database: {$e->getMessage()}"]);
+        exit;
+    }
+
+    if(isset($_GET['regionId'])){
+        $regionId = intval($_GET['regionId']);
+        $sql = "SELECT * FROM province WHERE region_id = $regionId";
+    }
+
+    try{
+        $provinces = $db->select_info_multiple_key($sql);
+
+        header('Content-Type: application/json');
+        echo json_encode($provinces);
+
+    } catch(Exception $e){
+        http_response_code(500);
+        echo json_encode(["error" => "Failed to fetch data: {$e->getMessage()}"]);
+        exit;
+    }
+
+?>
